9fc69b1eed6470ba52d9f2f73912c686c39bc681
ManuLasker <emanuel.afanador@koombea.com>
2021-04-20 13:45:08 -0500

    feat: implement routes for blending and mask model prediction

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,4 +1,5 @@
 import json
+from app.routes_models.models import blendModelRequest
 from app.utils import image as I
 from fastapi import Request, Depends
 from app.routes_models import *
@@ -23,6 +24,7 @@ def preprocess_image_route(body: cacheImageRequest,
                            db:Redis = Depends(get_redis_db)):
     logger.info('Caching base64 640x640 image encode and this size as the original size')
     # Reduce image to 640x640
+    logger.info('image id: ' + body.name_image)
     pil_image = I._base64_to_pil(body.base64_image)
     pil_image = I.resize_with_pad(pil_image, (640, 640))
     base64_image = I._pil_to_base64(pil_image)
@@ -63,11 +65,10 @@ def mask_grabcut_wm_helper_route(body: grabCutRequest, db:Redis = Depends(get_re
                                                                             mask_helper,
                                                                             original_size,
                                                                             resized_size)
-    # save mask
-    I._base64_to_pil(mask_b64).save('/mask.jpg')
     # Cache both paint mask
-    db.set('paintMaskOriginal', mask_b64)
-    db.set('paintMaskResized', mask_b64_resized)
+    db.set('paintMaskOriginal', json.dumps(mask_b64))
+    db.set('paintMaskResized', json.dumps(mask_b64_resized))
+    db.set('previewPaintMaskCut', json.dumps(preview_b64_resized))
     return {'mask': 'data:image/jpeg;base64'+','+mask_b64_resized,
             'preview_cut': 'data:image/jpeg;base64'+','+preview_b64_resized}
         
@@ -91,3 +92,63 @@ def mask_grabcut_square_helper_route(db:Redis = Depends(get_redis_db)):
     db.set('squareMaskResized', json.dumps(mask_b64_resized))
     return {'mask_rect': 'data:image/jpeg;base64'+','+mask_b64_resized,
             'preview_cut': 'data:image/jpeg;base64'+','+preview_b64_resized}
+    
+@app.post('/mask_model_prediction', description='Predict mask using dl model')
+def predict_mask_model(db: Redis = Depends(get_redis_db)):
+    # Get source image
+    src_image = json.loads(db.get('sourceImage'))
+    # Get sizes
+    original_size = json.loads(db.get('sourceImage''_original_size'))
+    resized_size = json.loads(db.get('sourceImage''_new_size'))
+    # predict mask
+    mask_b64, mask_b64_resized, preview_b64_resized = I.predict_mask_using_model(src_image,
+                                                                                original_size,
+                                                                                resized_size)
+    # cache all model mask
+    db.set('modelMaskOriginal', json.dumps(mask_b64))
+    db.set('modelMaskResized', json.dumps(mask_b64_resized))
+    db.set('modelPreviewCut', json.dumps(preview_b64_resized))
+   
+    return {'model_mask': 'data:image/jpeg;base64'+','+mask_b64_resized,
+            'preview_cut': 'data:image/jpeg;base64'+','+preview_b64_resized}
+
+@app.post('/blend_image', description='blend images using all the information')
+def blend(body: blendModelRequest, db: Redis = Depends(get_redis_db)):
+    # load src image information
+    src_image = json.loads(db.get('sourceImage'))
+    # load mask image information from grabcut
+    mask_image = json.loads(db.get('paintMaskOriginal'))
+    # load target information for the resized version
+    # load target image information
+    target_image = json.loads(db.get('targetImage''Resized'))
+    # load dims from target blending
+    dims = json.loads(db.get('xyxy_'+'targetImage'+'_coordinates'))
+    # load dimensions
+    # TODO
+    # blend images
+    b64original_blend, b64resized_blend = I.get_blending_image(src=src_image,
+                                                               mask=mask_image,
+                                                               target=target_image,
+                                                               dims=dims,
+                                                               naive=body.naive)
+    # save in cache
+    db.set('b64original_blend', json.dumps(b64original_blend))
+    if body.naive:
+        db.set('b64resized_blend_', json.dumps(b64resized_blend))
+    else:
+        db.set('b64resized_blend', json.dumps(b64resized_blend))
+    return {'finish_blend': True,
+            'blend_preview': 'data:image/jpeg;base64'+','+b64resized_blend} 
+    
+
+@app.get('/get_blend_image', description='get blend image to update view')
+def get_blend_image(db: Redis = Depends(get_redis_db)):
+    resized_blend_image = db.get('b64resized_blend')
+    resized_blend_image = (resized_blend_image if resized_blend_image is None 
+                            else 'data:image/jpeg;base64'+','+json.loads(resized_blend_image))
+    return {'blend_preview': resized_blend_image}
+
+@app.get('/get_preview_grabcut', description='get preview image for grabcut')
+def get_preview_image(db: Redis = Depends(get_redis_db)):
+    grabcutb64 = json.loads(db.get('previewPaintMaskCut'))
+    return {'preview_image': 'data:image/jpeg;base64'+','+grabcutb64}
