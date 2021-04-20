132086159ea3dcf53d6d64490ddb5f320cd4ef6f
ManuLasker <emanuel.afanador@koombea.com>
2021-04-20 13:44:52 -0500

    feat: add models config variables

--- src/app/config/variables.py
+++ src/app/config/variables.py
@@ -4,3 +4,5 @@ DEBUG = os.environ.get('DEBUG', False)
 REDIS_HOST = os.environ.get('REDIS_HOST')
 TITLE = 'Ai Pet Web app'
 DESCRIPTION = 'Web demo app for ai pet semi automatic process'
+MODELS_PATH = '/opt/models'
+MASK_MODEL_PATH = os.path.join(MODELS_PATH, 'torch.model_segmentation.pt')
