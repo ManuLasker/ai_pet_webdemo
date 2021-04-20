1722f1c5e3678a6039a16ff49eedb7923b686999
ManuLasker <emanuel.afanador@koombea.com>
2021-04-20 13:46:01 -0500

    feat: add new general utils and refactor

--- src/app/utils/general.py
+++ src/app/utils/general.py
@@ -1,4 +1,10 @@
+import pickle
+import json
+from typing import Tuple
+import app.utils.image as I
 import numpy as np
+import torch
+import app as app
 
 def xyxy2xywh(coords: np.ndarray) -> np.ndarray:
     """Transform coordinates from xyxy, (top-left, bottom-right)
@@ -14,6 +20,21 @@ def xyxy2xywh(coords: np.ndarray) -> np.ndarray:
     new_coords[1] = coords[1]
     new_coords[2] = np.abs(coords[0] - coords[2])
     new_coords[3] = np.abs(coords[1] - coords[3])
+    return new_coords
+
+def xywh2xyxy(coords: np.ndarray) -> np.ndarray:
+    """Transform coordinates from xywh, (top-left, width and height)
+    to xyxy, (top-left, bottom-right)
+    Args:
+        coords (np.ndarray): np.ndarray with xywh coordinates
+    Returns:
+        np.ndarray: numpy array with xyxy coordinates
+    """
+    new_coords = np.zeros_like(coords)
+    new_coords[0] = coords[0]
+    new_coords[1] = coords[1]
+    new_coords[2] = coords[0] + coords[2]
+    new_coords[3] = coords[1] + coords[3]
     return new_coords
 
 def scale_coordinates(new_shape: tuple, coordinates: np.ndarray,
@@ -41,3 +62,42 @@ def scale_coordinates(new_shape: tuple, coordinates: np.ndarray,
     # scale
     scale_coords /= r
     return scale_coords.round().astype(np.int32)
+
+def get_dimensions_box(coords: np.ndarray) -> Tuple[int, int]:
+    """Get Height and Width dimensions from xyxy coordinates
+    Args:
+        coords (np.ndarray): box coordinates in xyxy format
+    Returns:
+        Tuple[int, int]: Height and width
+    """
+    w, h = (np.abs(coords[0] - coords[2]), 
+            np.abs(coords[1] - coords[3]))
+    return int(h), int(w)
+
+def set_model_in_cache(**numpy_arrays_dict):
+    """Cache numpy arrays in a dict
+    """
+    db = list(app.get_redis_db())[0]
+    for key, numpy_array in numpy_arrays_dict.items():
+        # caching numpy arrays models
+        db.set(key, pickle.dumps(numpy_array))
+        
+def get_model_from_cache(*key_names) -> dict:
+    """get models from cache into a dict with the models arrays
+    Returns:
+        dict: dict with the models arrays from redis db
+    """
+    db = list(app.get_redis_db())[0]
+    models = {}
+    for key in key_names:
+        models[key] = pickle.loads(db.get(key))
+    return models
+
+def set_blend_img_in_cache(image: torch.Tensor) -> None:
+    """set blend image in cache db
+    Args:
+        image (torch.Tensor): image tensor
+    """
+    db = list(app.get_redis_db())[0]
+    db.set('b64resized_blend',
+           json.dumps(I._pil_to_base64(I._to_pil(image))))
