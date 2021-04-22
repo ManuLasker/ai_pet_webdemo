608a43c32ed3831b038550cf265d4244ee13ec7b
ManuLasker <emanuel.afanador@koombea.com>
2021-04-22 11:15:19 -0500

    feat: update hyperparameters

--- src/app/blending/utils.py
+++ src/app/blending/utils.py
@@ -154,8 +154,8 @@ def execute_blend_process(source_temp: torch.Tensor, mask_temp: torch.Tensor,
         
         # Algorithms configuration
         run = [0]
-        num_step = 1000
-        w_grad, w_cont, w_tv, w_style = 3e4, 2e1, 1e-6, 0.05
+        num_step = 500
+        w_grad, w_cont, w_tv, w_style = 3e4, 3e1, 1e-6, 0.05
         configurations = {
             'num_step': num_step,
             'alg config': {
